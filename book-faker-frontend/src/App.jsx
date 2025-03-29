5695cfb4c0e9c58c7d8824ddbb17d064a897b6bd
Sohelrana2815 <sohel152302@gmail.com>
2025-03-30 05:59:18 +0600

    created axiosIntance

--- book-faker-frontend/src/App.jsx
+++ book-faker-frontend/src/App.jsx
@@ -1,23 +1,26 @@
-import { useState,useEffect } from "react";
+import { useState, useEffect } from "react";
 import "./App.css";
-
+import axiosPublic from "./services/axiosPublic";
 function App() {
   const [books, setBooks] = useState([]);
   const [seed, setSeed] = useState(42);
   const [locale, setLocale] = useState("en_US");
 
-
   // FETCH DATA FROM API
-  
-  useEffect(()=>{
-    const fetchBooks = async()=>{
-      const response = await 
-    }
-  },[])
+
+  useEffect(() => {
+    const fetchBooks = async () => {
+      const response = await axiosPublic.get("/books", {
+        params: { seed, locale, startIndex: 0, count: 20 },
+      });
+      setBooks(response.data);
+    };
+    fetchBooks();
+  }, [seed, locale]);
 
   return (
     <>
-      <h1>Vite + React</h1>
+      <h1></h1>
     </>
   );
 }
