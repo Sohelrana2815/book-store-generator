86d9b75b60857cf69a62218037b9019b632550e4
Sohelrana2815 <sohel152302@gmail.com>
2025-03-30 11:26:53 +0600

    running the server

--- book-faker-backend/server.js
+++ book-faker-backend/server.js
@@ -1,17 +1,15 @@
 const express = require("express");
 const cors = require("cors");
-const bookController = require("./controllers/bookController");
 
 const app = express();
 
 // Middleware
+
 app.use(cors());
 app.use(express.json());
 
-// Routes
-app.get("/api/books", bookController.getBooks);
+const PORT = 5000;
 
-const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
