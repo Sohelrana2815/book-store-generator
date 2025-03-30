b4f7e85020bf3cb1c7c49d0bed63e6a5dd03dacd
Sohelrana2815 <sohel152302@gmail.com>
2025-03-30 15:44:35 +0600

    done

--- book-faker-backend/server.js
+++ book-faker-backend/server.js
@@ -1,15 +1,14 @@
 const express = require("express");
 const cors = require("cors");
+const bookRoutes = require("./routes/books");
 
 const app = express();
+app.use(cors()); // CORS এনাবল করুন
 
-// Middleware
-
-app.use(cors());
-app.use(express.json());
+// API রাউট কানেক্ট করুন
+app.use("/api/books", bookRoutes);
 
 const PORT = 5000;
-
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
