c1242ce3a6bd246ede1565eef8b088ac63826776
Sohelrana2815 <sohel152302@gmail.com>
2025-03-30 19:01:26 +0600

    done

--- book-faker-frontend/src/App.jsx
+++ book-faker-frontend/src/App.jsx
@@ -1,22 +1,10 @@
-import { useState, useEffect } from "react";
-import { useBooks } from "./hooks/useBooks";
-function App() {
-  const [locale, setLocale] = useState("en_US");
-  const [seed, setSeed] = useState(42);
-  const [avgLikes, setAvgLikes] = useState(3.7);
-  const [avgReviews, setAvgReviews] = useState(4.5);
 
-  
-
-  // FETCH DATA FROM API
-
+const App = () => {
   return (
-    <>
-      <div className="p-8">
-        <h1 className="text-2xl mb-4">Book Store Generator</h1>
-      </div>
-    </>
+    <div>
+      
+    </div>
   );
-}
+};
 
 export default App;
--- book-faker-frontend/src/main.jsx
+++ book-faker-frontend/src/main.jsx
@@ -4,9 +4,11 @@ import "./index.css";
 import App from "./App.jsx";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
+const queryClient = new QueryClient();
+
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <QueryClientProvider client={QueryClient}>
+    <QueryClientProvider client={queryClient}>
       <App />
     </QueryClientProvider>
   </StrictMode>
